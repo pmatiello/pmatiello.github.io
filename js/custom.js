68a579aa17da09b208657ebdb5488c3fe06f1c43
Pedro Matiello <pedro@pmatiello.me>
2017-06-19 20:38:31 -0300

    Use progressive enhancement for click to image.

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,7 @@
 function enableClickToImage() {
     var images = document.getElementsByTagName('img');
     Array.from(images).forEach(function(image) {
+        image.style.cursor = "pointer";
         image.addEventListener('click', function(event) {
             window.location = event.target.getAttribute('src');
         });
